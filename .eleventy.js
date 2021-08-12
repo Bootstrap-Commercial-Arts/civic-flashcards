ae30edfceb26171ebb45048832980955cdc81dca
bythebootstrap <schad@bythebootstrap.us>
2021-08-11 17:40:04 -0700

    added JS file, dropdown function, font weights

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventyGoogleFonts = require("eleventy-google-fonts");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('CSS');
+    eleventyConfig.addPassthroughCopy('JS');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPlugin(eleventyGoogleFonts);    
 };
